2ae457e9265440a1cd8c40ae93eb96f793ce71bd
straumat <github.backlog426@passmail.net>
2025-09-18 18:29:28 +0200

    Website fix for DO

--- src/public/javascript/app.js
+++
@@ -1,8 +0,0 @@
-const html = document.documentElement
-const topbar = document.getElementById("layout-topbar")
-if (topbar) {
-    const handleScroll = () =>
-        topbar.setAttribute("data-at-top", window.scrollY < 30 ? "true" : "false")
-    window.addEventListener("scroll", handleScroll, { passive: true })
-    handleScroll()
-}
